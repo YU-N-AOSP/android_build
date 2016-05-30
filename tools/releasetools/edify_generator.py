d81b11c5f113388d12904d808b0641f0c6d2f099
Louis Popi <theh2o64@gmail.com>
2016-05-30 19:24:50 +0200

    Revert "build: releasetools: ota: Add support for 32-64 bit upgrades"
    This reverts commit 543968904fd1823a35f8cc2e3a680907af3f816e.

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -140,7 +140,7 @@ class EdifyGenerator(object):
 
   def AssertDevice(self, device):
     """Assert that the device identifier is the given string."""
-    cmd = ('get_device_compatible("%s") == "OK" || '
+    cmd = ('getprop("ro.product.device") == "%s" || '
            'abort("This package is for \\"%s\\" devices; '
            'this is a \\"" + getprop("ro.product.device") + "\\".");') % (
                device, device)
